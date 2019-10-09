bb30c033c019c9d33f1a3d1e1651500e6f13247c
hjy951116 <50810033+hjy951116@users.noreply.github.com>
2019-10-09 13:03:22 +0200

    Add files via upload

--- 20191008.py
+++ 20191008.py
@@ -7,6 +7,7 @@ import glob
 import math
 from PIL import Image
 import sys
+import pandas as pd
 
 
 with open('./test.csv','r') as csvfile:
@@ -15,91 +16,109 @@ with open('./test.csv','r') as csvfile:
   column.pop(0)
   column = list(map(int,column))
 
-count2 = 0
-framey = []
-i = 0
-j = 0
-k = 0
-a = 0.2
 
-P = []
-Pl = 0
-Ph = 0
-F = numpy.zeros((720,1280))
-F1 = numpy.zeros((720,1280))
-# image = cv2.imread('000000.jpg')
-# Ec = image
-# Ec1 = image
-# Ec2 = image
-Ec = numpy.zeros((720,1280,3))
-Ec1 = numpy.zeros((720,1280,3))
-Ec2 = numpy.zeros((720,1280,3))
+deltay = numpy.zeros((720,1280))
+deltau = numpy.zeros((720,1280))
+deltav = numpy.zeros((720,1280))
+ye = numpy.zeros((720,1280))
+ue = numpy.zeros((720,1280))
+ve = numpy.zeros((720,1280))
+
+
+k = 0
+
+image = cv2.imread('./1/000000.jpg')
+# gray_levels = 256
+# previous_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+yuv1 = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+previous_gray = yuv1[...,0]
+previous_U = yuv1[...,1]  #0.492 * (img[...,0] - gray)
+previous_V = yuv1[...,2]
+# previous_U = 0.492 * (image[...,0] - previous_gray)
+# previous_V = 0.877 * (image[...,2] - previous_gray)
+
 
 count = 0
 
 # Open frames in the folder
 for frames in glob.glob('./test/*.jpg'):
+  a = []
+  b = []
+  c = []
   img = cv2.imread(frames)
-  ycbcr = Image.open(frames)
-  y, cb, cr = ycbcr.split()
-  y.show()
-  Y = numpy.array(y)
-#   print(Y)
-  gray_levels = 256
-  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
-  hist = numpy.histogram(Y,bins=gray_levels)
-  cv2.imwrite('%.1d-gray.jpg'%count,gray)
-  
+#   gray_levels = 256
+#   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+  gray = yuv[...,0]
+  U = yuv[...,1]  #0.492 * (img[...,0] - gray)
+  V = yuv[...,2]  #0.877 * (img[...,2] - gray)
+
+  for i in range (720):
+    for j in range (1280):
+    #   a.append(gray[i][j])
+    #   b.append(previous_gray[i][j])
+      if gray[i][j] > previous_gray[i][j]:
+        deltay[i][j] = gray[i][j] - previous_gray[i][j]
+        ye[i][j] = 1
+      else:
+        deltay[i][j] = previous_gray[i][j] - gray[i][j]
+        ye[i][j] = -1
+      if U[i][j] > previous_U[i][j]:
+        deltau[i][j] = U[i][j] - previous_U[i][j]
+        ue[i][j] = 1
+      else:
+        deltau[i][j] = previous_U[i][j] - U[i][j]
+        ue[i][j] = -1
+      if V[i][j] > previous_V[i][j]:
+        deltav[i][j] = V[i][j] - previous_V[i][j]
+        ve[i][j] = 1
+      else:
+        deltav[i][j] = previous_V[i][j] - V[i][j]
+        ve[i][j] = -1
+    #   c.append(ye[i][j]*deltay[i][j])
+        
+  previous_gray = gray
+  previous_U = U
+  previous_V = V
+
   # print(hist)
   im = Image.open(frames)
   pix = im.load()
-  cv2.imwrite('%.1d.jpg'%count,img)
-#   r = img[...,2]
-#   g = img[...,1]
-#   b = img[...,0]
   width = im.size[0]
   height = im.size[1]
-  
-  dst = cv2.bilateralFilter(Y, d=10, sigmaColor=100, sigmaSpace=100)
-  cv2.imwrite('%.6d.jpg'%count,dst)
-  Ym = numpy.mean(Y)
-  Yrange = numpy.max(Y)-numpy.min(Y)
-  YA = 0.5 + Ym - dst
-#   print(Ym)
-#   print(YA)
+  pixelindex = numpy.zeros((720,1280))
 
   if column[k] == 1:
-    for j in range (255):
-      f = hist[0]
-      P.append(float(f[j])/float(921600))
-      w = abs(j-127)/255 + 0.7
-      if j < 255-Ym :
-        Pl += w*float(f[j])/float(921600)
-      else:
-        Ph += w*float(f[j])/float(921600)
     for m in range (720):
       for n in range (1280):
-        if Y[m][n] < Ym:
-          F[m][n] = YA[m][n]*(sum(P[:int(255-Y[m][n])]))/Pl
-          F1[m][n]=255-F[m][n]
-          for c in range (3):
-            Ec1[m][n][c] = img[m,n,c]*F1[m][n]/Y[m][n]
-            Ec2[m][n][c] = (Ec1[m][n][c]+img[m,n,c]+F1[m][n]-Y[m][n])/2
-            Ec[m][n][c] = Ec2[m][n][c] + (img[m,n,c]-Y[m][n])*(Ym/Yrange)
-        else:
-        # print(YA[x][y])
-        # print(sum(P))
-        # print(sum(P[:int(255-gray[x][y])]))
-        # print(Ph)
-          F[m][n] = YA[m][n]+(1-YA[m][n])*(sum(P)-sum(P[:int(255-Y[m][n])]))/Ph
-          F1[m][n]=255-F[m][n]
-          for c in range (3):
-            Ec1[m][n][c] = img[m,n,c]*F1[m][n]/Y[m][n]
-            Ec2[m][n][c] = (Ec1[m][n][c]+img[m,n,c]+F1[m][n]-Y[m][n])/2
-            Ec[m][n][c] = Ec2[m][n][c] + (img[m,n,c]-Y[m][n])*(Ym/Yrange)
-    cv2.imwrite('%.6d-comp.jpg'%count,F1)
-    print(Ec)
-    cv2.imwrite('%.6d-const.jpg'%count,Ec)
+        # pixelindex[m][n] = ye[m][n]*deltay[m][n]
+        # pixelindex[m][n] = ue[m][n]*deltau[m][n]
+        pixelindex[m][n] = ve[m][n]*deltav[m][n]
+
+        if deltay[m][n] > 10 and ye[m][n] > 0 and ve[m][n] < 0 and deltau[m][n] > 4 and ue[m][n] > 0:          
+          img[m,n,2] =  img[m,n,2] - ye[m][n]*deltay[m][n] - ve[m][n]*1.14*deltav[m][n]
+          img[m,n,1] =  img[m,n,1] - ye[m][n]*deltay[m][n] + ue[m][n]*0.395*deltau[m][n] + ve[m][n]*0.581*deltav[m][n]
+          img[m,n,0] =  img[m,n,0] - ye[m][n]*deltay[m][n] - ue[m][n]*2.033*deltau[m][n]
+        # elif deltay[m][n] > 40:
+        #   img[m,n,2] =  img[m,n,2] - deltay[m][n] + 1.14*10
+        #   img[m,n,1] =  img[m,n,1] - deltay[m][n] + 0.395*8 - 0.581*10
+        #   img[m,n,0] =  img[m,n,0] - deltay[m][n] - 2.033*8
+        # elif deltay[m][n] > 10:
+        #   img[m,n,2] =  img[m,n,2] - deltay[m][n] + 1.14*4
+        #   img[m,n,1] =  img[m,n,1] - deltay[m][n] + 0.395*8 - 0.581*4
+        #   img[m,n,0] =  img[m,n,0] - deltay[m][n] - 2.033*8
+    # dataframe = pd.DataFrame({'a_name':a,'b_name':b,'c_name':c})
+    # dataframe.to_csv("result.csv",index=False,sep=',')
+    print(k,'flash')
+    cv2.imwrite('%.1d-comp.jpg'%count,img)
+    count += 1
+    plt.imshow(numpy.flipud(pixelindex),interpolation='nearest',cmap='bone',origin='lower')
+    plt.colorbar()
+    plt.xticks(())
+    plt.yticks(())
+    plt.show()
+  else:
+    print(k,'no flash')
     
 
      
